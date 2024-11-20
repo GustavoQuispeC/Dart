f463ad7960125ba899645a12c289e9528dbc8805
Gustavo <gusstavocta@gmail.com>
2024-11-20 01:46:21 -0500

    first commit

---
+++ 01-DART-INTRO/01_hello_world.dart
@@ -0,0 +1,11 @@
+void main() {
+  //var myName = 'Gustavo';
+  //late final myName = 'Gustavo';
+  //String  myName = 'Gustavo';
+  
+  const myName = 'Gustavo';
+
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${1 + 1}');
+}
