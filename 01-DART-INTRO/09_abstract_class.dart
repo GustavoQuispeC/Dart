939105db3523f3281a754ca8b8be4c02a9b9383e
Gustavo <gusstavocta@gmail.com>
2024-11-22 00:45:23 -0500

    bastract class extends implements

--- 01-DART-INTRO/09_abstract_class.dart
+++ 01-DART-INTRO/09_abstract_class.dart
@@ -1,7 +1,19 @@
 void main(){
   
-//     final windPlant = EnergyPlant();
+   final windPlant = WindPlant(initialEnergy: 100);
+  final nuclearPlant = NuclearPlant(energyLeft: 1000);                                                                                                                                                                                                                                         
   
+  print(windPlant);
+  
+  print('wind: ${chargePhone(windPlant)}');
+  print('Nuclear: ${chargePhone(nuclearPlant)}');
+}
+
+double chargePhone(EnergyPlant plant){
+  if(plant.energyLeft < 10){
+    throw Exception('No hay suficiente energia');
+  }
+  return plant.energyLeft - 10;
 }
 
 enum PlantType { nuclear, wind, water }
@@ -11,7 +23,7 @@ enum PlantType { nuclear, wind, water }
 abstract class EnergyPlant{
   
   double energyLeft;
-  PlantType type; //nuclear, wind, water
+  final PlantType type; //nuclear, wind, water
   
   EnergyPlant({
     required this.energyLeft,
@@ -19,4 +31,31 @@ abstract class EnergyPlant{
   });
   
   void consumeEnergy( double amount);
+}
+
+//extends o implements
+class WindPlant extends EnergyPlant{
+  
+  WindPlant({required double initialEnergy})
+    : super( energyLeft: initialEnergy, type: PlantType.wind );
+  
+  @override
+  void consumeEnergy( double amount ){
+    energyLeft -= amount;
+  }
+}
+
+class NuclearPlant implements EnergyPlant{
+   @override
+  double energyLeft;
+  
+   @override
+  final PlantType type = PlantType.nuclear;
+  
+  NuclearPlant({required this.energyLeft});
+  
+  @override
+  void consumeEnergy( double amount ){
+    energyLeft -= (amount * 0.5);
+  }
 }
