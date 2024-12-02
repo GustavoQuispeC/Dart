15a36a2d93e2856f0aa15ac66b02a2cccf460d66
Gustavo <gusstavocta@gmail.com>
2024-12-02 00:07:08 -0500

    implementando mensajes

--- yes_no_app/lib/presentation/providers/chat_provider.dart
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messageList = [
     const Message(text: 'Hola Laura!', fromWho: FromWho.me),
     const Message(
         text: 'Como has estado, donde te presentaras?', fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage(String text) async {
-    message.add(Message(text: text, fromWho: FromWho.me));
+    messageList.add(Message(text: text, fromWho: FromWho.me));
     notifyListeners();
   }
 }
